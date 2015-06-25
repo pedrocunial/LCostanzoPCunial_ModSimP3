d7c8a74ad2d6fc5b6e7b691302528dcd7d17c73c
pedrocunial <pedrocc4@al.insper.edu.br>
2015-06-25 11:33:53 -0300

    funfa mtas coisas!!
    é incrível, não??

--- iteracao.py
+++ iteracao.py
@@ -7,6 +7,7 @@ from numpy import linspace, pi
 from math import sin, cos, degrees, asin, acos 
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import numpy as np
 
 massa = 0.33	#massa da pedra, será desconsiderada na rotação
 hDavid = 1.57	#alturas
@@ -22,9 +23,9 @@ diametro = 0.06	#diametro da pedra
 aAngular = 1
 mao = 0.5
 
-T = linspace(0,5,1001)
+T = linspace(0,8,1001)
 
-def func1(V,T):
+def rotacao(V,T):
 	global sina,cosa
 	global dxpdt,dypdt
 	theta = (aAngular*T*T)/2 	#definição do angulo atual (em radianos) em função da aceleração angular e tempo 
@@ -41,26 +42,39 @@ def func1(V,T):
 	vpp = 0
 
 	dxpdt = vpf * cosa 
-	dypdt = vpf * sina 
+	dypdt = vpf * sina
+	print(dxpdt,dypdt) 
 	
 	return [dxpdt,dypdt]
 
 M0 = [mao,-L]
-
-M = odeint(func1,M0,T)
+M = odeint(rotacao,M0,T)
 
 print(dxpdt, dypdt)
 
 #Os valores de seno e cosseno finais são tirados da função, por trigonometria, pode-se provar que o valor o seno de lançamento é -cos do momento, assim como o cosseno do lançamento é -sen do momento.
 
-cosLancamento = -sina
-senLancamento = -cosa
-S = []
+def lancamento(V,T):
+	dxdt = dxpdt * 10
+	dydt = dypdt * 10 - g*(T-8)
 
-while 
+	return dxdt,dydt
+
+
+L0 = [0,hLancamento]
+T = linspace(8,13.15,101)
+L = odeint(lancamento,L0,T)
+
 
 
 plt.title('Espaço')
 plt.plot(M[:,0],M[:,1],'g')
 plt.show()
 
+plt.title('Lançamento')
+plt.plot(L[:,0],L[:,1],'r')
+plt.show()
+
+# plt.title('SERÁ??!')
+# plt.plot(S[0],S[1],'b')
+# plt.show()
